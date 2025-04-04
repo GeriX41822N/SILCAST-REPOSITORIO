d6d6bf372694a97b80bb5fde6201a39fa730a4c2
Gerino Mendez Nicasio <gerino.mendez193@gmail.com>
2025-04-04 13:48:17 -0600

    modificacion del encabezado funcional

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Importa RouterModule
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule], // Agrega RouterModule a los imports
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
