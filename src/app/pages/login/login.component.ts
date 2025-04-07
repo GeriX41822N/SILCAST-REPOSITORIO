a335ab5a223b44d3d50414bafa3422497ac0c8bf
Gerino Mendez Nicasio <gerino.mendez193@gmail.com>
2025-04-07 10:39:17 -0600

    feat: se agregó el logo de la empresa al login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // 👈 Importa FormsModule aquí
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule], // 👈 Agrégalo aquí
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  onLogin() {
+    console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.password);
+    // Aquí puedes conectar después con Laravel
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(FormsModule) // 👈 ¡Esto es lo que faltaba!
+  ]
 }).catch(err => console.error(err));
-
